95b51e82f767b002515bed8068409368371e859b
Spandan <spandanchakraborty@users.noreply.github.com>
2017-04-30 16:53:09 -0700

    Add files via upload

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -117,11 +117,12 @@ class LearningAgent(Agent):
         # When learning, check if the 'state' is not in the Q-table
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
-        if not state in self.Q:
-            temp_act_dict={}
-            for x in self.valid_actions:
-                temp_act_dict[x]=0.0
-            self.Q[state]=temp_act_dict
+        if self.learning:
+            if not state in self.Q:
+                temp_act_dict={}
+                for x in self.valid_actions:
+                    temp_act_dict[x]=0.0
+                self.Q[state]=temp_act_dict
 
         return
 
